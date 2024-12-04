5c3cfa4d5b34e25f8d4c2ba6f619875b4ebcc4a7
Justy <justynaonceagainforgothermail@gmail.com>
2024-12-04 08:18:10 +0100

    Part 3 - products mvt

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,9 @@ from .models import Product, Customer, Order
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
 from rest_framework import viewsets
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import ProductForm
 
 
 def hello_world(request):
@@ -20,3 +23,20 @@ class CustomerViewSet(viewsets.ModelViewSet):
 class OrderViewSet(viewsets.ModelViewSet):
      queryset = Order.objects.all()
      serializer_class = OrderSerializer
+
+class ProductListView(ListView):
+     model = Product
+     template_name = 'product_list.html'
+     context_object_name = 'products'
+class ProductDetailView(DetailView):
+     model = Product
+     template_name = 'product_detail.html'
+     context_object_name = 'product'
+
+class ProductCreateView(CreateView):
+     model = Product
+     form_class = ProductForm
+     template_name = 'product_create.html'
+     success_url = '../../products/'
+     def form_valid(self, form):
+          return super().form_valid(form)
