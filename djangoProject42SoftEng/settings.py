5c34e4c04e460ae0410131e457d8f289e2664c95
Justy <justynaonceagainforgothermail@gmail.com>
2025-01-24 21:53:18 +0100

    fixed db settings

--- djangoProject42SoftEng/settings.py
+++ djangoProject42SoftEng/settings.py
@@ -81,28 +81,29 @@ WSGI_APPLICATION = 'djangoProject42SoftEng.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.postgresql',
-# 'NAME': os.getenv('DATABASE_NAME'),
-# 'USER': os.getenv('DATABASE_USER'),
-# 'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-# 'HOST': os.getenv('DATABASE_HOST', 'localhost'),
-# 'PORT': os.getenv('DATABASE_PORT', 5432),
-# }
-# }
 DATABASES = {
-    'default': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': BASE_DIR / 'db.sqlite3',
-          'ENGINE': 'django.db.backends.postgresql',
-          'NAME': 'Software_Engineering',
-          'USER': 'Justyna',
-          'PASSWORD': 'Love2231',
-          'HOST': 'localhost',
-          'PORT': '5432',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': os.getenv('DATABASE_NAME'),
+'USER': os.getenv('DATABASE_USER'),
+'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+'PORT': os.getenv('DATABASE_PORT', 5432),
 }
+}
+
+# DATABASES = {
+#     'default': {
+#           'ENGINE': 'django.db.backends.sqlite3',
+#           'NAME': BASE_DIR / 'db.sqlite3',
+#           'ENGINE': 'django.db.backends.postgresql',
+#           'NAME': 'Software_Engineering',
+#           'USER': 'Justyna',
+#           'PASSWORD': 'Love2231',
+#           'HOST': 'localhost',
+#           'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
