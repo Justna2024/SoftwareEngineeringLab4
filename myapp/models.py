ba86d112ac9e4605f301fa079aae04b5455c0ec6
Justy <justynaonceagainforgothermail@gmail.com>
2025-01-24 23:02:51 +0100

    adding small error

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Product(models.Model):
      id = models.AutoField(primary_key=True)
-     name = models.CharField(max_length=255) # here changed to 50
+     product_name = models.CharField(max_length=255) # here changed to 50
      price = models.DecimalField(max_digits=20, decimal_places=2)
      available = models.BooleanField()
 
